936b37275f54e70e0bba1c7d5ef86e7bc08545b7
chen412115 <412115725@qq.com>
2020-03-02 10:23:26 +0800

    Update AMapBaseMapPlugin.kt

--- map/android/src/main/kotlin/me/yohom/amapbasemap/AMapBaseMapPlugin.kt
+++ map/android/src/main/kotlin/me/yohom/amapbasemap/AMapBaseMapPlugin.kt
@@ -49,6 +49,7 @@ class AMapBaseMapPlugin {
                                         arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION,
                                                 Manifest.permission.ACCESS_FINE_LOCATION,
                                                 Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                                                "android.permission.ACCESS_BACKGROUND_LOCATION",
                                                 Manifest.permission.READ_EXTERNAL_STORAGE,
                                                 Manifest.permission.READ_PHONE_STATE),
                                         permissionRequestCode
